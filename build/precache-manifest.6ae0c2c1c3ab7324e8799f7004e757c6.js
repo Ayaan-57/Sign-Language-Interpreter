eda57ac9a21c1084f325d49ee0bee5eecbf36ecc
Rajita Roy <68264150+rajitaroy@users.noreply.github.com>
2021-11-24 19:25:10 +0530

    Add files via upload

---
+++ build/precache-manifest.6ae0c2c1c3ab7324e8799f7004e757c6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f5f71959addc9d43201be324eec12c0f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2307b63eab0eea1af768",
+    "url": "/static/css/main.523f09c5.chunk.css"
+  },
+  {
+    "revision": "783cd63646c98e8d0699",
+    "url": "/static/js/2.08731b47.chunk.js"
+  },
+  {
+    "revision": "927d15cad02b8fa4f913e8e01503c169",
+    "url": "/static/js/2.08731b47.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2307b63eab0eea1af768",
+    "url": "/static/js/main.000f2899.chunk.js"
+  },
+  {
+    "revision": "949eb168f1d833ae83d5",
+    "url": "/static/js/runtime-main.743d20cc.js"
+  }
+]);
